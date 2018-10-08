6a804904ab0babd9252ef11f00e68f13460a5e43
lounceny2 <lounceny2@gmail.com>
2018-10-08 18:10:06 +0000

    My commit of 08/10/2018 18:09pm

---
+++ src/main/java/com/myschool/config/DBConfig.java
@@ -0,0 +1,65 @@
+package com.myschool.config;
+
+import java.util.Properties;
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import static org.hibernate.cfg.AvailableSettings.*;
+import static org.hibernate.cfg.AvailableSettings.C3P0_MAX_STATEMENTS;
+
+
+@Configuration
+@PropertySource("classpath:dev-db.properties")
+@EnableTransactionManagement
+public class DBConfig {
+    @Autowired
+    private Environment env;
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+        try {
+
+            factoryBean.setDataSource(getDataSource());
+            factoryBean.setPackagesToScan("com.myschool.domain");
+            factoryBean.setHibernateProperties(hibernateProperties());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return factoryBean;
+    }
+    @Bean
+    public DataSource getDataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(env.getProperty("mysql.driver"));
+        dataSource.setUrl(env.getProperty("mysql.jdbcUrl"));
+        dataSource.setUsername(env.getProperty("mysql.username"));
+        dataSource.setPassword(env.getProperty("mysql.password"));
+        return dataSource;
+    }
+    @Bean
+    public PlatformTransactionManager transactionManager(){
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory().getObject());
+        return transactionManager;
+    }
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.put(DIALECT, env.getProperty("hibernate.dialect"));
+        properties.put(HBM2DDL_AUTO, env.getProperty("hibernate.hbm2ddl.auto"));
+        properties.put(SHOW_SQL, env.getProperty("hibernate.show_sql"));
+        properties.put(C3P0_MIN_SIZE, env.getProperty("hibernate.c3p0.min_size"));
+        properties.put(C3P0_MAX_SIZE, env.getProperty("hibernate.c3p0.max_size"));
+        properties.put(C3P0_ACQUIRE_INCREMENT, env.getProperty("hibernate.c3p0.acquire_increment"));
+        properties.put(C3P0_TIMEOUT, env.getProperty("hibernate.c3p0.timeout"));
+        properties.put(C3P0_MAX_STATEMENTS, env.getProperty("hibernate.c3p0.max_statements"));
+        return properties;
+    }
+}
---
+++ src/main/java/com/myschool/config/WebConfig.java
@@ -0,0 +1,30 @@
+package com.myschool.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.myschool"})
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry){
+        registry.jsp().prefix("/WEB-INF/views/").suffix(".jsp");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/index").setViewName("index");
+    }
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+    }
+
+
+}
